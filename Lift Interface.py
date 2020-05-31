92fe98a3e44d71cd82f56c20dd05cc2e86ba2f6e
Will-Harris00 <wjharris00@outlook.com>
2020-05-31 22:46:11 +0100

    Draw Floors

--- Lift Interface.py
+++ Lift Interface.py
@@ -2,58 +2,52 @@ from tkinter import *
 from tkinter import messagebox
 
 
-class root(Frame):
-    def __init__(self, master):
-        self.master = master
+class model(Frame):
+    def __init__(self, root):
+        self.root = root
 
-        self.canvas = Canvas(self.master, width=500, height=500, borderwidth=0,
-                             highlightthickness=0, bg="lightblue")
+        self.canvas = Canvas(self.root, width=500, height=500, borderwidth=0, highlightthickness=0,
+                             bg="lightblue")
         self.canvas.pack(fill="both", expand="true")
 
         self.error = StringVar()
-        self.errorLabel = Label(self.master, textvariable=self.error)
         self.error.set('No errors currently detected.')
+        self.errorLabel = Label(self.root, textvariable=self.error)
         self.errorLabel.pack(side="bottom", fill='both')
 
-        self.numFloors = IntVar()
-        self.numPeople = IntVar()
-        self.numFloors.set(10)
-        self.numPeople.set(100)
+        # self.numFloors = IntVar(value=10)
+        # self.numPeople = IntVar(value=100)
 
-        self.floorsString = StringVar()
-        self.floorsString.set("Floors :")
-        self.floorsLabel = Label(self.master, textvariable=self.floorsString)
+        self.floorsLabel = Label(self.root, text="Floors :")
         self.floorsLabel.pack(side="left")
-        self.inputNumFloors = Entry(self.master, textvariable=self.numFloors, width=3)
+        self.inputNumFloors = Entry(self.root, textvariable="100", width=3)
         self.inputNumFloors.pack(side="left")
 
-        self.peopleString = StringVar()
-        self.peopleString.set("People :")
-        self.peopleLabel = Label(self.master, textvariable=self.peopleString)
+        self.peopleLabel = Label(self.root, text="People: ")
         self.peopleLabel.pack(side="left", padx=4, pady=2)
-        self.inputNumPeople = Entry(self.master, textvariable=self.numPeople, width=3)
+        self.inputNumPeople = Entry(self.root, textvariable="10", width=3)
         self.inputNumPeople.pack(side="left", pady=2)
 
         self.info = StringVar()
-        self.infoLabel = Label(self.master, textvariable=self.info)
+        self.infoLabel = Label(self.root, textvariable=self.info)
         self.info.set('Information: ')
         self.infoLabel.pack(side="left", fill='both', padx=5, pady=2)
 
-        self.startBtn = Button(self.master, text="Run", fg="green", width=5,
+        self.startBtn = Button(self.root, text="Run", fg="green", width=5,
                                command=lambda: self.validate())
         self.startBtn.pack(side="right", padx=4, pady=2)
-        self.autocompleteBtn = Button(self.master, text="Autocomplete", fg="blue")
+        self.autocompleteBtn = Button(self.root, text="Autocomplete", fg="blue")
         self.autocompleteBtn.pack(side="right", padx=4, pady=2)
 
     def validate(self):
         self.numFloors = int(self.inputNumFloors.get())
         self.numPeople = int(self.inputNumPeople.get())
-        if self.numFloors < 2 or self.numFloors > 25:
+        if self.numFloors < 2 or self.numFloors >= 25:
             self.error.set(
-                "The number of floors '" + self.inputNumPeople.get() + "' is out of range. Please choose a number between 2 and 30.")
+                "The number of floors '" + self.inputNumPeople.get() + "' is out of range. Please choose a number between 2 and 25.")
             print(
-                "The number of floors '" + self.inputNumPeople.get() + "' is out of range. Please choose a number between 2 and 30.")
-        elif self.numPeople < 1 or self.numPeople > 200:
+                "The number of floors '" + self.inputNumPeople.get() + "' is out of range. Please choose a number between 2 and 25.")
+        elif self.numPeople < 1 or self.numPeople >= 200:
             self.error.set(
                 "The number of people '" + self.inputNumFloors.get() + "' is out of range. Please choose a number between 1 and 200.")
             print(
@@ -62,24 +56,31 @@ class root(Frame):
             self.inputNumFloors.config(state="disabled")
             self.inputNumPeople.config(state="disabled")
             self.error.set('No errors currently detected.')
-            root.draw(self)
+            model.draw()
 
     def draw(self):
-        self.canvas.create_rectangle(0, 0, 100, 100)
+        for i in range(self.numFloors):
+            self.canvas.create_line(0, self.canvas.winfo_height() / self.numFloors * i + 1,
+                                    self.canvas.winfo_width(),
+                                    self.canvas.winfo_height() / self.numFloors * i + 1,
+                                    fill="burlywood", width=2)
 
 
 def on_closing():
     if messagebox.askokcancel("Exit program", "Do you want to quit?"):
-        master.destroy()
+        root.destroy()
 
 
 if __name__ == "__main__":
-    master = Tk()
-    master.title("Lift Manager")
-    Frame = root(master)
-    master.protocol("WM_DELETE_WINDOW", on_closing)
-    master.minsize(350, 300)
+    root = Tk()
+    root.title("Lift Manager")
+    model = model(root)
+
+    root.protocol("WM_DELETE_WINDOW", on_closing)
+    root.geometry("600x625+525+15")
+    root.resizable(False, False)
+
     # root.pack_propagate(1)
     # root.update_idletasks()
-    master.mainloop()
+    root.mainloop()
 
