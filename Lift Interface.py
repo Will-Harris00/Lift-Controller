e1ce3eb8735d66a6b60ea7984c87b00d4e6c2633
Will-Harris00 <wjharris00@outlook.com>
2020-05-31 22:57:07 +0100

    Floor Alignment

--- Lift Interface.py
+++ Lift Interface.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from tkinter import messagebox
-import random
 
 
 class model(Frame):
@@ -16,12 +15,15 @@ class model(Frame):
         self.errorLabel = Label(self.root, textvariable=self.error)
         self.errorLabel.pack(side="bottom", fill='both')
 
-        self.floorsLabel = Label(self.root, text="Storeys: ")
+        # self.numFloors = IntVar(value=10)
+        # self.numPeople = IntVar(value=100)
+
+        self.floorsLabel = Label(self.root, text="Floors :")
         self.floorsLabel.pack(side="left")
         self.inputNumFloors = Entry(self.root, textvariable="100", width=3)
         self.inputNumFloors.pack(side="left")
 
-        self.peopleLabel = Label(self.root, text="People: ")
+        self.peopleLabel = Label(self.root, text="People :")
         self.peopleLabel.pack(side="left", padx=4, pady=2)
         self.inputNumPeople = Entry(self.root, textvariable="10", width=3)
         self.inputNumPeople.pack(side="left", pady=2)
@@ -36,32 +38,26 @@ class model(Frame):
         self.startBtn.pack(side="right", padx=4, pady=2)
         self.autocompleteBtn = Button(self.root, text="Autocomplete", fg="blue")
         self.autocompleteBtn.pack(side="right", padx=4, pady=2)
-        self.liftTiles = {}
-        self.peopleList = []
+        self.lift_pos = {}
 
     def validate(self):
         self.numFloors = int(self.inputNumFloors.get())
         self.numPeople = int(self.inputNumPeople.get())
-        if self.numFloors < 2 or self.numFloors > 25:
+        if self.numFloors < 2 or self.numFloors > 10:
             self.error.set(
-                "The number of floors '" + self.inputNumFloors.get() + "' is out of range. Please choose a number between 2 and 10.")
+                "The number of floors '" + self.inputNumPeople.get() + "' is out of range. Please choose a number between 2 and 10.")
             print(
-                "The number of floors '" + self.inputNumFloors.get() + "' is out of range. Please choose a number between 2 and 10.")
+                "The number of floors '" + self.inputNumPeople.get() + "' is out of range. Please choose a number between 2 and 10.")
         elif self.numPeople < 1 or self.numPeople > 50:
             self.error.set(
-                "The number of people '" + self.inputNumPeople.get() + "' is out of range. Please choose a number between 1 and 50.")
+                "The number of people '" + self.inputNumFloors.get() + "' is out of range. Please choose a number between 1 and 50.")
             print(
-                "The number of people '" + self.inputNumPeople.get() + "' is out of range. Please choose a number between 1 and 50.")
+                "The number of people '" + self.inputNumFloors.get() + "' is out of range. Please choose a number between 1 and 50.")
         else:
             # self.inputNumFloors.config(state="disabled")
             # self.inputNumPeople.config(state="disabled")
             self.error.set('No errors currently detected.')
             model.draw()
-
-        tile = self.liftTiles[3]
-        tile_color = self.canvas.itemcget(tile, "fill")
-        new_color = "pink" if tile_color == "NavajoWhite" else "pink"
-        self.canvas.itemconfigure(tile, fill=new_color)
 
     def draw(self):
         self.canvas.delete("nums")
@@ -70,7 +66,7 @@ class model(Frame):
         cellwidth = int(self.canvas.winfo_width() / 3)
         cellheight = int(round(self.canvas.winfo_height() / self.numFloors))
         for column in range(3):
-            current_floor = self.numFloors
+            current_floor = self.numFloors - 1
             if column in range(1, 3, 2):
                 for row in range(self.numFloors):
                     x1 = column * cellwidth
@@ -82,63 +78,28 @@ class model(Frame):
                     self.canvas.create_line(0, y1, self.canvas.winfo_width(),
                                             y1, fill="burlywood",
                                             tags="divs")
-                    liftPosition = self.canvas.create_rectangle(x1, y1, x2, y2,
-                                                                fill="NavajoWhite",
-                                                                tags="flrs")
-                    self.liftTiles[current_floor] = liftPosition
+                    lift_pos = self.canvas.create_rectangle(x1, y1, x2, y2,
+                                                            fill="NavajoWhite",
+                                                            tags="flrs")
+                    self.lift_pos[current_floor] = lift_pos
                     current_floor -= 1
             elif column in range(0, 3, 2):
                 floor_num = 0
-                for row in range(self.numFloors + 1, 0, -1):
+                for row in range(self.numFloors, 0, -1):
+                    # x1 = column * cellwidth
                     y1 = (row * cellheight) - 5
                     if row % 2 == 0:
                         y1 -= 1
+                    # x2 = x1 + (cellwidth // cellwidth)
                     y2 = y1 - (cellheight // 1.5)
-                    self.canvas.create_text(1, y2, anchor="nw", text=str(floor_num), tags="flrs",
-                                            font=('Arial', -round(cellheight // 1.75)))
+                    if floor_num == 0:
+                        self.canvas.create_text(1, y2, anchor="nw", text="G", tags="nums",
+                                                font=('Arial', -round(cellheight // 1.75)))
+                    elif floor_num != 0:
+                        self.canvas.create_text(1, y2, anchor="nw", text=str(floor_num),
+                                                tags="flrs",
+                                                font=('Arial', -round(cellheight // 1.75)))
                     floor_num += 1
-        model.genPeople()
-
-    def genPeople(self):
-        for i in range(1, self.numPeople + 1):
-            self.peopleList.append(people(self.numFloors))
-        print(self.peopleList)
-        print(self.liftTiles)
-
-
-class people(object):
-    def __init__(self, numFloors):
-        selection = []
-        start = random.randint(0, numFloors)
-        selection += (list(range(0, start)))
-        selection += (list(range(start + 1, numFloors + 1)))
-        end = random.choice(selection)
-        if end > start:
-            self.direction = "UP"
-        else:
-            self.direction = "DOWN"
-        self.originFlr = start
-        self.destFlr = end
-        print("\n" + str(self.originFlr))
-        print(self.direction)
-        print(self.destFlr)
-
-
-class elevator(object):
-    def __init__(self, root):
-        self.capacity = 6
-        self.floor = 1
-        self.direction = "UP"
-        # elevator.move(self, 1)
-
-    """
-    def move(self, position):
-        print(root)
-        tile = root.liftTiles[position]
-        tile_color = root.canvas.itemcget(tile, "fill")
-        new_color = "pink" if tile_color == "NavajoWhite" else "pink"
-        root.canvas.itemconfigure(tile, fill=new_color)
-    """
 
 
 def on_closing():
@@ -149,9 +110,7 @@ def on_closing():
 if __name__ == "__main__":
     root = Tk()
     root.title("Lift Manager")
-
     model = model(root)
-    lift = elevator(root)
 
     root.protocol("WM_DELETE_WINDOW", on_closing)
     root.geometry("600x625+525+15")
@@ -160,3 +119,4 @@ if __name__ == "__main__":
     # root.pack_propagate(1)
     # root.update_idletasks()
     root.mainloop()
+
