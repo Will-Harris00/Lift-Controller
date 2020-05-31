ae8d278ae920a70a8cfe16f87407ffbcc5166f73
Will-Harris00 <wjharris00@outlook.com>
2020-05-31 21:17:06 +0100

    Ground Floor Start

--- Lift.py
+++ Lift.py
@@ -1,4 +1,5 @@
 import tkinter as root
+from tkinter import messagebox
 import random
 class Window(root.Tk):
     def __init__(self, *args, **kwargs):
@@ -15,13 +16,15 @@ class Window(root.Tk):
         self.canvas.bind("<Configure>", self.redraw)
         self.status = root.Label(self, anchor="w")
         self.status.pack(side="bottom", fill="x")
+        self.running = False
+        self.position = 0
 
 
     def redraw(self, event=None):
         self.canvas.delete("lfts")
         self.canvas.delete("flrs")
-        cellwidth = int(self.canvas.winfo_width()/self.columns)
-        cellheight = int(self.canvas.winfo_height()/self.rows)
+        cellwidth = int(self.canvas.winfo_width() / self.columns)
+        cellheight = int(self.canvas.winfo_height() / self.rows)
         for column in range(self.columns):
             current_floor = self.top_floor
             if column in range(1, (self.columns), 2):
@@ -33,22 +36,22 @@ class Window(root.Tk):
                     lift_pos = self.canvas.create_rectangle(x1,y1,x2,y2, fill="lightblue", tags="lfts")
                     self.lift_pos[self.num_lifts, current_floor] = lift_pos
                     current_floor -= 1
-            elif column in range(0, (self.columns-1), 2):
+            elif column in range(0, (self.columns - 1), 2):
                 floor_num = 0
                 for row in range(self.rows, 0, -1):
                     x1 = column * cellwidth
                     y1 = row * cellheight
-                    x2 = x1 + (cellwidth//1.5)
-                    y2 = y1 - (cellheight//1.5)
+                    x2 = x1 + (cellwidth // 1.5)
+                    y2 = y1 - (cellheight // 1.5)
                     if floor_num == 0:
                         self.canvas.create_text(x2, y2, text="G", tags="flrs",
-                                                font=('Arial', -round(cellheight//1.75)))
+                                                font=('Arial', -round(cellheight // 1.75)))
                     elif floor_num != 0:
                         self.canvas.create_text(x2, y2, text=str(floor_num), tags="flrs",
                                                 font=('Arial', -round(cellheight // 1.75)))
                     floor_num += 1
-        self.travel()
         self.movement()
+
 
     def travel(self):
         self.dest = []
@@ -56,26 +59,23 @@ class Window(root.Tk):
         end = random.randint(0, self.top_floor)
         self.dest.append(start)
         self.dest.append(end)
+        print(self.dest)
         return self.dest
 
 
     def movement(self):
-        print(self.dest)
-        if self.dest[0] == self.dest[1]:
-            del self.dest[1], self.dest[0]
-            self.travel()
-        elif self.dest[0] < self.dest[1]:
-            for i in range(self.dest[0],self.dest[1]):
-                continue
-        else:
-            for i in range(self.dest[0], self.dest[1], -1):
-                continue
-        position = self.lift_pos[self.num_lifts, self.dest[0]]
-        lift_color = self.canvas.itemcget(position, "fill")
+        self.lift = self.lift_pos[self.num_lifts, self.position]
+        lift_color = self.canvas.itemcget(self.lift, "fill")
         new_color = "lightblue" if  lift_color == "lightpink" else "lightpink"
-        self.canvas.itemconfigure(position, fill=new_color)
+        self.canvas.itemconfigure(self.lift, fill=new_color)
+
+
+def on_closing():
+    if messagebox.askokcancel("Exit program", "Do you want to quit?"):
+        window.destroy()
 
 
 if __name__ == "__main__":
     window = Window()
+    window.protocol("WM_DELETE_WINDOW", on_closing)
     window.mainloop()
