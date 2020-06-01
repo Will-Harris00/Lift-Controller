91f344210d3ece5c7f8b5adc516855ef5c439118
Will-Harris00 <wjharris00@outlook.com>
2020-06-01 21:54:11 +0100

    List Concatenation

--- Lift Logic.py
+++ Lift Logic.py
@@ -7,35 +7,34 @@ class Building(object):
         Building.move(self)
 
     def move(self):
-        for i in range(20):
-            Building.check(self)
-            if lift.liftFloor == numFloors - 1 or 0:
+        for i in range(22):
+            if lift.liftFloor > numFloors - 1 or lift.liftFloor < 0:
                 lift.direction *= -1
-                lift.liftFloor += lift.direction
+                lift.liftFloor += lift.direction * 2
             else:
+                print("The lift is on floor: " + str(lift.liftFloor))
+                Building.check(self)
                 lift.liftFloor += lift.direction
-
 
     def check(self):
-        for person in floorsList[lift.liftFloor - 1].peopleOnFloor:
+        for person in floorsList[lift.liftFloor].peopleOnFloor:
             print(person.idPerson)
-            print(person)
 
 
 class Lift(object):
     def __init__(self):
         self.capacity = 6
-        self.liftFloor = 1
-        self.direction = 1
+        self.liftFloor = 0
+        self.direction = -1
         self.passenger = []
 
 
 class People(object):
     def __init__(self, numFloors, personId):
-        selection = []
         start = random.randint(0, numFloors)
-        selection += (list(range(0, start)))
-        selection += (list(range(start + 1, numFloors + 1)))
+        # This concatenates two list either side of the originating floor of the passenger
+        selection = ((list(range(0, start))) + (list(range(start + 1, numFloors + 1))))
+        # The destination floor is selected from a list of available floors excluding the start.
         end = random.choice(selection)
         self.idPerson = personId
         if end > start:
@@ -54,7 +53,7 @@ class Floors(object):
         self.idFloor = floorId
         self.count = 0
         self.peopleOnFloor = Floors.assign(self, peopleList)
-        print("Number of people on floor " + str(self.idFloor) + " is " + str(len(self.peopleOnFloor)))
+        print("Floor " + str(self.idFloor) + " has " + str(len(self.peopleOnFloor)) + " passengers waiting.")
 
     def assign(self, peopleList):
         self.peopleOnFloor = []
